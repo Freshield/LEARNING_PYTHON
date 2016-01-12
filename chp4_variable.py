ba85d039e52202df7a3174c3616ffaff996626a1
Freshield <zxdsw199182@gmail.com>
2016-01-11 19:00:15 -0500

    chp4_variable

---
+++ chp4_variable.py
@@ -0,0 +1,52 @@
+cd MEGA\LEARN_BYSELF\PYTHON
+
+#print absolute value of an integer:
+a = 100
+if a>= 0:
+    print(a)
+else:
+    print(-a)
+	
+print('I\'m ok.')
+print('I\'m learning\nPython.')
+print('\\\n\\')
+print('\\\t\\')
+print(r'\\\t\\')
+
+age = 22
+if age >= 18:
+    print('adult')
+else:
+    prtin('tennager')
+	
+a = 1
+t_007 = 'T007'
+Answer = True
+
+a = 123
+print(a)
+a = 'ABC'
+print(a)
+
+x = 10
+x = x + 2
+print(x)
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+print(10 / 3)
+
+print(9/3)
+
+print(10 // 3)
+print(10 % 3)
+
+print('n = ',123,'\nf = ',456.789,'\ns1 = \'Hello, world\'\ns2 = \'Hello, \\\'Adam\\\'\'\ns3 = r\'Hello, \"Bart\"\'\ns4 = r\'\'\'Hello,\nLisa!\'\'\'')
+
+s5 = '''Hello,
+233!'''
+print(s1,'\n',s2,'\n',s3,'\n',s4,'\n',s5)
+
