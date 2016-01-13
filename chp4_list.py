b3f6c1e1fbea855879321bbb706f61a9d552abc6
Freshield <zxdsw199182@gmail.com>
2016-01-12 20:37:38 -0500

    chp4 list

---
+++ chp4_list.py
@@ -0,0 +1,103 @@
+classmates = ['michael','bob','tracy']
+classmates
+len(classmates)
+
+classmates[0]
+classmates[1]
+classmates[2]
+
+classmates[-1]
+classmates[-2]
+classmates[-3]
+
+classmates.append('adam')
+classmates
+
+classmates.insert(1,'jack')
+classmates
+
+classmates.pop()
+classmates
+
+classmates.pop(1)
+classmates
+
+classmates[1] = 'sarah'
+classmates
+
+L = ['apple',123,True]
+L
+
+s = ['python','java',['asp','php'],'scheme']
+len(s)
+
+z = [s,L]
+z
+
+p = ['asp233','php']
+s = ['python','java',p,'scheme']
+
+L = []
+len(L)
+
+classmates = ('michael','bob','tracy')
+classmates
+classmates[0]
+classmates[-1]
+classmates = []
+
+t = (1,2)
+t
+
+t = ()
+t
+
+t = (1)
+t
+
+t = (1,)
+t
+
+t = ('a','b',['A','B'])
+t
+t[2][0] = 'x'
+t[2][1] = 'y'
+t
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
