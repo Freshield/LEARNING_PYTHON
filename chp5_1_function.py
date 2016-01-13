828291bab63d5befd897ef68bc23c43373460af6
Freshield <zxdsw199182@gmail.com>
2016-01-12 23:36:59 -0500

    chp5 function

---
+++ chp5_1_function.py
@@ -0,0 +1,58 @@
+pi = 3.14
+r1 = 12.34
+r2 = 9.08
+r3 = 73.1
+s1 = pi*r1*r1
+s2 = pi*r2*r2
+s3 = pi*r3*r3
+print(s1,s2,s3)
+
+help(abs)
+
+print(abs(100))
+print(abs(-20))
+print(abs(12.14))
+
+print(max(1,2))
+print(max(2,3,1,-5))
+
+print(int('123'))
+print(int(12.34))
+print(float('12.34'))
+print(str(1.23))
+print(str(100))
+print(bool(1))
+print(bool(''))
+
+a = abs
+print(a(-1))
+
+
+
+n1 = 255
+n2 = 1000
+
+print(hex(n1))
+print(hex(n2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
