643b3e64c65340d14cafea2f0965924f510576ad
Freshield <zxdsw199182@gmail.com>
2016-01-12 22:00:46 -0500

    chp4 loop

---
+++ chp4_loop.py
@@ -0,0 +1,44 @@
+1 + 2 + 3
+
+names = ['michael','bob','tracy']
+for name in names:
+    print(name)
+	
+sum = 0
+for x in [1,2,3,4,5,6,7,8,9,10]:
+    sum = sum + x
+print(sum)
+
+print(list(range(5)))
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+
+sum = 0
+n = 99
+while n>0:
+    sum = sum + n
+    n = n - 2
+print(sum)
+
+L = ['bart','lisa','adam']
+n = 0
+while n<=2:
+    print(L[n])
+    n = n + 1
+for x in L:
+    print(x)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
