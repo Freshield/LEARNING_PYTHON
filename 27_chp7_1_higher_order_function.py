e513c18f5a9476e92085ce435d2dff0a6f1fd46a
Freshield <zxdsw199182@gmail.com>
2016-01-16 19:34:32 -0500

    27 higher order function

---
+++ 27_chp7_1_higher_order_function.py
@@ -0,0 +1,44 @@
+#27/123
+
+print(abs(-10))
+
+print(abs)
+
+x = abs(-10)
+
+print(x)
+
+f = abs
+
+print(f)
+
+print(f(-10))
+
+#abs=10
+
+print(abs(-10))
+
+def add(x,y,f):
+    return print(f(x) + f(y))
+
+first_value=int(input('please input the first value:'))
+second_value=int(input('please input the second value:'))
+
+add(first_value,second_value,abs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
